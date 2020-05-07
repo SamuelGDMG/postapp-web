3a99a30dd9a246d63ac4b097f08bcf6864fba703
Samuel <samuel.santos134@etec.sp.gov.br>
2020-05-06 22:14:07 -0300

    Fix ant-design

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('antd', {
+    libraryDirectory: 'es',
+   style: true,
+  }),
+ addLessLoader({
+   lessOptions: {
+     javascriptEnabled: true,
+     modifyVars: { '@primary-color': '#1DA57A' },
+   },
+ }),
+);
