ba0345066710265b0f90dfbfda15111d76130c1d
gutovesco <guto.vesco@hotmail.com>
2020-05-09 22:43:46 -0300

    login post structure

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import { Button, Input, Row } from 'antd';
 import {Container, AnimationContainer, Background} from './styles'
 import { MailOutlined, LockOutlined, UserOutlined, ArrowRightOutlined, LoginOutlined } from '@ant-design/icons';
 import './styles.less'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api'
 
 const SignIn = () => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('')
+    const history = useHistory()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = useCallback(async (e) => {
         e.preventDefault()
 
-        console.log(name)
-        console.log(email)
-        console.log(password)
+        try{
+
+        const data = {
+            name: name,
+            password: password,
+            email: email
+        }
+
+        const response = await api.post('/users', data);
+
+        const {token} = response.data
+
+        localStorage.setItem('token', token);
+
+        history.push('/home')
+    }catch(err){
+        console.log(err)
     }
+
+    }, [history, email, name, password])
 
     return (
         <>
